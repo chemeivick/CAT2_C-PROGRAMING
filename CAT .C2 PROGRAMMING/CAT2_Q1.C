668a368f42cf2d51e26bcc92646b6867dc480a53
chemeivick <victarchemei@gmail.com>
2024-11-07 03:46:17 -0800

    Update CAT2_Q1.C

--- CAT .C2 PROGRAMMING/CAT2_Q1.C
+++ CAT .C2 PROGRAMMING/CAT2_Q1.C
@@ -1,19 +1,28 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<Project>
-	<Libs Value=""/>
-	<Flags Value=""/>
-	<Target Value="CAT2_Q1.exe"/>
-	<CommandLine Value=""/>
-	<CompilerOptions Value="-Wall -s"/>
-	<DeleteObjectsBefore Value="Yes"/>
-	<DeleteObjectsAfter Value="Yes"/>
-	<DeleteMakefileAfter Value="Yes"/>
-	<DeleteResourcesAfter Value="Yes"/>
-	<EnableTheme Value="No"/>
-	<RequiresAdmin Value="No"/>
-	<CompilerType Value="CPP"/>
-	<AppType Value="CONSOLE"/>
-	<Files>
-		<File Name="main.c"/>
-	</Files>
-</Project>
+//structure program
+#include<stdio.h>
+#include<string.h>
+struct employee;//declearing structure variables
+{
+char name[50];
+int id;
+char department[20];
+float salary;
+char email[50];
+}employee
+//main function
+int main()
+{
+struct employee1
+strcpy(employee1.name,"john doe");
+employee1.id=12345;
+strcpy(employee1.department,"Human resources");
+employee1.salary=55000.50;
+strcpy(employee1.email,"john.doe@company.com");
+//output display
+printf("%s",employee1.name);
+printf("%d",employee1.id);
+printf("%s",employee1.department);
+printf("%f",employee1.salary);
+printf("%s",employee1.email);
+return o;
+}
